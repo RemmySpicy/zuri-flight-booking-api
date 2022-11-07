e980c564c2596b67c0ad053001bbfe15f81de884
O.Remilekun <remmylknolaotan@gmail.com>
2022-11-07 14:10:06 +0100

    Add flight controller code

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,18 +1,137 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
-}
+const {
+    uuid
+} = require("uuidv4");
+const {
+    flights
+} = require("../models/flights");
 
-const Flight =  require('../models/Flight');
+const {
+    vString,
+    vNumber,
+    vDateString,
+    formatDate
+} = require('./utility/validator')
 
-exports.getFlight = async (req, res) => {
+exports.getAllFlights = async (req, res, next) => {
     try {
-        const flights = Flight;
+        const time = req.query.time
+        let Flights = [...flights]
+        if (time) Flights = Flights.filter(flight => flight.time === time)
         res.status(200).json({
-            message: "all flights",
-            flight: flight,
-          });
+            flights: Flights,
+        });
     } catch (error) {
-        res.status(500).json({ message: error})
+        res.status(500).json({
+            status: "failed",
+            message: "Internal server error",
+        });
     }
-}
+};
+
+exports.getSingleFlight = async (req, res, next) => {
+    try {
+        const flightId = req.params.id;
+        const flight = flights.find(flight => flightId === flight.id);
+        if (!flight)
+            return res.status(404).json({
+                status: "failed",
+                message: "flight not found",
+            });
+        res.status(200).json({
+            data: flight,
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Internal server error",
+        });
+    }
+};
+
+exports.addFlight = async (req, res, next) => {
+    try {
+        const flight = await req.body;
+        if (vString(flight.title) && vString(flight.time) && vDateString(flight.date) && vNumber(flight.price)) {
+            flight.date = formatDate(flight.date)
+            flight.id = uuid();
+            flights.push(flight);
+            return res.status(200).json({
+                status: "success",
+                message: "flight added successfully",
+                data: flight,
+            });
+        }
+        res.status(400).json({
+            status: "failed",
+            message: "bad request",
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Internal server error",
+        });
+    }
+};
+
+exports.updateFlight = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const flight = flights.find(flight => flight.id === id);
+        if (!flight) {
+            return res.status(404).json({
+                status: "failed",
+                message: "flight not found",
+            });
+        }
+        const update = req.body;
+        const updated = {};
+        updated.title = update.title ? update.title : flight.title;
+        updated.time = update.time ? update.time : flight.time;
+        updated.price = update.price ? formatDate(update.price) : flight.price;
+        updated.date = update.date ? update.date : flight.date;
+        updated.id = flight.id;
+        if (!(vString(updated.title) && vString(updated.time) && vDateString(updated.date) && vNumber(updated.price))) {
+        return res.status(400).json({
+            status: "failed",
+            message: "bad request",
+        })
+        }
+        const index = flights.indexOf(flight);
+        flights.splice(index, 1, updated);
+        res.json({
+            status: "succes",
+            message: "flight updated succesfully",
+            data: updated,
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Internal server error",
+        });
+    }
+};
+
+exports.deleteFlight = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const flight = flights.find(flight => flight.id === id);
+        if (!flight) {
+            return res.status(404).json({
+                status: "failed",
+                message: "flight not found",
+            });
+        }
+        const index = flights.indexOf(flight);
+        flights.splice(index, 1);
+        res.status(200).json({
+            status: "success",
+            message: "flight deleted successfully",
+            data: flight,
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Internal server error",
+        });
+    }
+};
